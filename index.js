5a87ad2b3f903768fb5a585e6bc58183222be104
liam <liam@nehoray.net>
2024-04-15 14:39:47 +0200

    Added a header with a title. Added three buttos for rock paper scissors. Added a styles.css file to style these html elements. Put the index.js code into a init so DOM elements will only be fetched once the page has fully loaded

--- index.js
+++ index.js
@@ -1,78 +1,61 @@
-const choices = ['rock', 'paper', 'scissors']
+window.onload = init;
 
-function getComputerChoice() {
-    computerChoice = choices[Math.floor(Math.random() * choices.length)]
+function init() {
 
-    return computerChoice
-}
+    const choices = ['rock', 'paper', 'scissors']
 
-function playRound(playerSelection, computerSelection) {
-    const win = computerSelection + "! You win (:"
-    const lose = computerSelection + "! You lose ):"
-    const tie = computerSelection + "! We tied |:"
-    const noChoice = 'Please enter rock paper or scissors'
-
-    playerSelection = playerSelection.toLowerCase()
-
-    if (playerSelection != 'rock' && playerSelection != 'paper' && playerSelection != 'scissors') {
-        return noChoice
+    function getComputerChoice() {
+        computerChoice = choices[Math.floor(Math.random() * choices.length)]
+    
+        return computerChoice
     }
-
-    //Logic for rock slection
-    if (playerSelection == 'rock' && computerSelection == 'paper') {
-        return lose
-    } else if (computerSelection == 'scissors') {
-        return win
-    } else if (computerSelection == 'rock') {
-        return tie
-    }
-
-    //Logic for paper selection
-    if (playerSelection == 'paper' && computerSelection == 'scissors') {
-        return lose
-    } else if (computerSelection == 'rock') {
-        return win
-    } else if (computerSelection == 'paper') {
-        return tie
-    }
-
-    //Logic for scissors selection
-
-    if (playerSelection == 'scissors' && computerSelection == 'rock') {
-        return lose
-    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-        return win
-    } else if (playerSelection == 'scissors' && computerSelection == 'scissors') {
-        return tie
-    }
-}
-
-
-
-function playGame(){
-    let score = 0
-    for(i = 0; i<5;){
-        playerSelection = prompt()
-        computerSelection = getComputerChoice()
-
-        let roundResult = playRound(playerSelection, computerSelection)
-        if(roundResult.includes('win') == true){
-            score++
-        } else if(roundResult.includes('lose') == true){
-            score--
+    
+    function playRound(playerSelection, computerSelection) {
+        const win = computerSelection + "! You win (:"
+        const lose = computerSelection + "! You lose ):"
+        const tie = computerSelection + "! We tied |:"
+        const noChoice = 'Please enter rock paper or scissors'
+    
+        playerSelection = playerSelection.toLowerCase()
+    
+        if (playerSelection != 'rock' && playerSelection != 'paper' && playerSelection != 'scissors') {
+            return noChoice
         }
-
-        console.log(roundResult)
-        console.log(score)
-        i++
+    
+        //Logic for rock slection
+        if (playerSelection == 'rock' && computerSelection == 'paper') {
+            return lose
+        } else if (computerSelection == 'scissors') {
+            return win
+        } else if (computerSelection == 'rock') {
+            return tie
+        }
+    
+        //Logic for paper selection
+        if (playerSelection == 'paper' && computerSelection == 'scissors') {
+            return lose
+        } else if (computerSelection == 'rock') {
+            return win
+        } else if (computerSelection == 'paper') {
+            return tie
+        }
+    
+        //Logic for scissors selection
+    
+        if (playerSelection == 'scissors' && computerSelection == 'rock') {
+            return lose
+        } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+            return win
+        } else if (playerSelection == 'scissors' && computerSelection == 'scissors') {
+            return tie
+        }
     }
-    if (score > 0) {
-        console.log('You won the most rounds, you win the game!')
-    } else if (score < 0) {
-        console.log('You lost to many games ): I win!')
-    }
+    
+    const selectorButton = document.querySelector('#rock')
+    
+    console.log(selectorButton)
+    // selectorButton.addEventListener('click', () => {
+    //     console.log(selectorButton.id)
+    // })
+    
 }
-
-playGame()
-
-
